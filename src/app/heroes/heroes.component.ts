c547c34b1b0c9e449a3ce8ac9972a53472c8887a
75asa <75asa.me@gmail.com>
2022-03-01 09:02:21 +0900

    :sparkles: list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,12 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  // hero = 'Windstorm';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
   constructor() {}
 
   ngOnInit(): void {}
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
